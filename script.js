9580972fb5cdb6d98fb4e7e882467de63c577631
Eliza Tan <elizacarrietan2006@gmail.com>
2025-03-29 17:24:13 -0400

    setting up node.js and starter chat code. let's hope and pray.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+//hello this is openai api vibes
+const OpenAI = require("openai");
+require("dotenv").config();
 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+exports.chatReq = async (req, res) => {
+    try {
+      const message = "Hi chat! Can you help me with diagnosing something from symptoms?";
+      const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: message }],
+        temperature: 0,
+        max_tokens: 1000,
+      });
+      res.status(200).json(response);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  };
